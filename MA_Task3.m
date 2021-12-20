a9601b704a0082493db731d50e304d2ecb2a9975
ksiat <96122336+ksiat@users.noreply.github.com>
2021-12-20 04:16:33 +0200

    Update MA_Task3.m

--- MA_Task3.m
+++ MA_Task3.m
@@ -69,3 +69,131 @@ for i=1:length(rusers)
     [B,I]=mink(similarityList(rusers(i),:),groupSize-1);
     divergentTeams(:,i)=[rusers(i) I];
 end
+
+%rerun task 2a
+TopN = 500;
+NoItems=500;
+opts.DataRange = '2:501';
+items_filename = 'C:\Users\Kyriakos\Desktop\polloi007\ergasia\items.xls';
+items_mv = readmatrix(items_filename,opts);
+
+[pref_list,ratings]=findPrefTable(users_mv,items_mv,TopN,NoUsers,NoItems);
+bordaItem=zeros(NoItems,NumOfGroups);%for each g in vector
+for k=1:NumOfGroups % for each team compute the borda score
+    for u=1:groupSize %for each user
+        user=similarTeams(u,k);
+        for i=1:NoItems % for each item in their list
+            pref_item=pref_list(i,user);
+            score=NoItems-i;
+            bordaItem(pref_item,k)=bordaItem(pref_item,k)+score;
+        end
+    end
+end
+
+[~,IB] = max(bordaItem);
+
+
+copelandItem=zeros(NoItems,NumOfGroups);%for each g in vector
+for k=1:NumOfGroups % for each team compute the copeland score
+    for u=1:groupSize %for each user compare every two items
+        user=similarTeams(u,k);
+        user_pref_list=pref_list(:,user);
+        for i=1:NoItems-1 % for each item in their list except the last one compare with
+            for j=i+1:NoItems % every other item except i
+                i_place=find(user_pref_list==i);
+                j_place=find(user_pref_list==j);
+                if (i_place<j_place)
+                    copelandItem(i,k)=copelandItem(i,k)+1;
+                elseif(j_place<i_place)
+                    copelandItem(j,k)=copelandItem(j,k)+1;     
+                else 
+                    copelandItem(i,k)=copelandItem(i,k)+0.5;
+                    copelandItem(j,k)=copelandItem(j,k)+0.5;
+                end
+            end
+        end
+    end
+end
+[~,IC] = max(copelandItem);
+
+SBavgScoret=zeros(1,NumOfGroups);%similar teams borda average score table
+for sg=1:length(similarTeams)   %for each group
+    tempavgScore=0.0;
+    for u=1:groupSize   %for each user
+        tempavgScore=tempavgScore+ratings(IB(sg),similarTeams(u,sg));
+    end
+    SBavgScoret(sg)=tempavgScore/groupSize;
+end
+    
+SCavgScoret=zeros(1,NumOfGroups);%similar teams copeland average score table
+for sg=1:length(similarTeams)   %for each group
+    tempavgScore=0.0;
+    for u=1:groupSize   %for each user
+        tempavgScore=tempavgScore+ratings(IC(sg),similarTeams(u,sg));
+    end
+    SCavgScoret(sg)=tempavgScore/groupSize;
+end
+
+bordaItem=zeros(NoItems,NumOfGroups);%for each g in vector
+for k=1:NumOfGroups % for each team compute the borda score
+    for u=1:groupSize %for each user
+        user=divergentTeams(u,k);
+        for i=1:NoItems % for each item in their list
+            pref_item=pref_list(i,user);
+            score=NoItems-i;
+            bordaItem(pref_item,k)=bordaItem(pref_item,k)+score;
+        end
+    end
+end
+
+[~,IB] = max(bordaItem);
+
+
+copelandItem=zeros(NoItems,NumOfGroups);%for each g in vector
+for k=1:NumOfGroups % for each team compute the copeland score
+    for u=1:groupSize %for each user compare every two items
+        user=divergentTeams(u,k);
+        user_pref_list=pref_list(:,user);
+        for i=1:NoItems-1 % for each item in their list except the last one compare with
+            for j=i+1:NoItems % every other item except i
+                i_place=find(user_pref_list==i);
+                j_place=find(user_pref_list==j);
+                if (i_place<j_place)
+                    copelandItem(i,k)=copelandItem(i,k)+1;
+                elseif(j_place<i_place)
+                    copelandItem(j,k)=copelandItem(j,k)+1;     
+                else 
+                    copelandItem(i,k)=copelandItem(i,k)+0.5;
+                    copelandItem(j,k)=copelandItem(j,k)+0.5;
+                end
+            end
+        end
+    end
+end
+[~,IC] = max(copelandItem);
+
+
+
+DBavgScoret=zeros(1,NumOfGroups);%divergent teams borda average score table
+for sg=1:length(divergentTeams)   %for each group
+    tempavgScore=0.0;
+    for u=1:groupSize   %for each user
+        tempavgScore=tempavgScore+ratings(IB(sg),divergentTeams(u,sg));
+    end
+    DBavgScoret(sg)=tempavgScore/groupSize;
+end
+    
+DCavgScoret=zeros(1,NumOfGroups);%divergent teams copeland average score table
+for sg=1:length(divergentTeams)   %for each group
+    tempavgScore=0.0;
+    for u=1:groupSize   %for each user
+        tempavgScore=tempavgScore+ratings(IC(sg),divergentTeams(u,sg));
+    end
+    DCavgScoret(sg)=tempavgScore/groupSize;
+end
+
+%Average score of each mechanism
+sbAvgScore=mean(SBavgScoret);
+scAvgScore=mean(SCavgScoret);
+dbAvgScore=mean(DBavgScoret);
+dcAvgScore=mean(DCavgScoret);

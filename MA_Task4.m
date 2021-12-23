01f694b561eedfde427195ba02c04998d0ddd5f3
Gperakis2 <giorgosperakis1998@gmail.com>
2021-12-23 12:55:14 +0200

    Update MA_Task4.m

--- MA_Task4.m
+++ MA_Task4.m
@@ -1,8 +1,5 @@
 clear all;
 close all;
-
-
-
 %%%%%%%%%%%%%%excel Open%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sheet = 1;
 
@@ -21,71 +18,38 @@ NoUsers=g(4);
 NoItems=500;
 
 teams=randi([0 999],NoUsers,NumOfGroups);
-budget_of_group=0
+group_budget=0;
 
-%%%Count the sum of budgets of the chosen team
+%%%Find total budget of the chosen team
 for k=1:NoUsers
     user=teams(k);
     user_budget=users_mv(user+1,11);
-      budget_of_group=  user_budget +budget_of_group;
+    group_budget=  user_budget +group_budget;
+    budgets(k,1) =user_budget
 end
 
-
-%%%%Find the items that can be bought with the budget of the group and put
-%%%%them in a array%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5%% 
-feasible_items=[]
+%%%%Find the items that can be bought with the budget of the group 
+feasible_items=[];
 for k=1:NoItems
-    
-    if items_mv(k,11)<=budget_of_group
-        feasible_items=[feasible_items;items_mv(k,:)]
+    if items_mv(k,11)<=group_budget
+        feasible_items=[feasible_items;items_mv(k,:)];
     end    
 end
 
-
-%%%Choose one random item
-ind = +ceil(rand * size(feasible_items,1));
-sel_item = feasible_items(ind,:);
-
-
-
-%%KL divergence algorithm for each user with this item
- NoItems=1
-D=8;
+      
+ budget_cost=0
+ item_cost=1
+ i=0
+ while(budget_cost<item_cost)
+     [sel_item,r]=recursiveFunction(users_mv,NoUsers,feasible_items)
+     c=r/10
+     item_cost=sel_item(11)
+     budget_cost=c*budgets 
+     i=i+1
+ end
  
-    rmax=10;
-    
-    COV_u = 2*eye(D);
-    COV_i = eye(D);
-    
-    r=zeros(NoItems,NoUsers);
-    
-    for u=1:NoUsers
-       for i=1:NoItems
-           KL=0.5*log(det(inv(COV_u)*COV_i))+0.5*trace(inv(inv(COV_u)*COV_i))-0.5*D +0.5*(users_mv(u,[3:10])-items_mv(i,[3:10]))*inv(COV_i)*transpose(users_mv(u,[3:10])-items_mv(i,[3:10]));
-           r(i,u) = rmax - (KL/rmax);     
-       end
-    end
- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    
- %%%%%%%Mechanism for distributing the cost that each player should pay
-    ratings=sum(r)
-    rmax=max(r)
-    item_cost=sel_item(11)
-    %group_ratings=[r;tranverse(teams)]
-    
-    group_budget=0
-   for k=1:NoUsers
-    user=teams(k);
-    user_budget=users_mv(user+1,10);
-    group_budget=user_budget+group_budget
-   
-   end 
-   
-   for k=1:NoUsers
-    user=teams(k);
-    user_budget=users_mv(user+1,10);
-    x(k)=(user_budget/group_budget)*item_cost
-   
-   end 
-ra=sum(x)
+ if budget_cost>item_cost
+    weight=(item_cost)/budget_cost
+    x(:,1)=c*budgets(:,1)*weight
+    y=sum(x)
+ end
